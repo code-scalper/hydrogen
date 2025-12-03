c95a7521b47c4372b69317b04c1665e8d6c91380
code-scalper <tigerbaram@gmail.com>
2025-12-03 22:08:31 +0900

    'asd'

--- dist-electron/preload.mjs
+++ dist-electron/preload.mjs
@@ -1,1 +1,40 @@
-"use strict";const n=require("electron");n.contextBridge.exposeInMainWorld("ipcRenderer",{on(...e){const[r,o]=e;return n.ipcRenderer.on(r,(t,...i)=>o(t,...i))},off(...e){const[r,...o]=e;return n.ipcRenderer.off(r,...o)},send(...e){const[r,...o]=e;return n.ipcRenderer.send(r,...o)},invoke(...e){const[r,...o]=e;return n.ipcRenderer.invoke(r,...o)}});n.contextBridge.exposeInMainWorld("electronAPI",{runExe:e=>n.ipcRenderer.invoke("run-exe",e),stopExe:()=>n.ipcRenderer.invoke("stop-exe"),readRecentLogs:()=>n.ipcRenderer.invoke("read-recent-logs"),readOutputData:e=>n.ipcRenderer.invoke("read-output-data",e),readProgressLog:e=>n.ipcRenderer.invoke("read-progress-log",e),readEconomicEvaluation:e=>n.ipcRenderer.invoke("read-economic-evaluation",e),downloadReportFiles:e=>n.ipcRenderer.invoke("download-report-files",e),downloadOutputTotal:e=>n.ipcRenderer.invoke("download-output-total",e)});n.contextBridge.exposeInMainWorld("electronStore",{get:e=>n.ipcRenderer.invoke("electron-store-get",e),set:(e,r)=>n.ipcRenderer.invoke("electron-store-set",e,r),delete:e=>n.ipcRenderer.invoke("electron-store-delete",e)});
+"use strict";
+const electron = require("electron");
+electron.contextBridge.exposeInMainWorld("ipcRenderer", {
+  on(...args) {
+    const [channel, listener] = args;
+    return electron.ipcRenderer.on(
+      channel,
+      (event, ...args2) => listener(event, ...args2)
+    );
+  },
+  off(...args) {
+    const [channel, ...omit] = args;
+    return electron.ipcRenderer.off(channel, ...omit);
+  },
+  send(...args) {
+    const [channel, ...omit] = args;
+    return electron.ipcRenderer.send(channel, ...omit);
+  },
+  invoke(...args) {
+    const [channel, ...omit] = args;
+    return electron.ipcRenderer.invoke(channel, ...omit);
+  }
+  // You can expose other APTs you need here.
+  // ...
+});
+electron.contextBridge.exposeInMainWorld("electronAPI", {
+  runExe: (payload) => electron.ipcRenderer.invoke("run-exe", payload),
+  stopExe: () => electron.ipcRenderer.invoke("stop-exe"),
+  readRecentLogs: () => electron.ipcRenderer.invoke("read-recent-logs"),
+  readOutputData: (payload) => electron.ipcRenderer.invoke("read-output-data", payload),
+  readProgressLog: (payload) => electron.ipcRenderer.invoke("read-progress-log", payload),
+  readEconomicEvaluation: (payload) => electron.ipcRenderer.invoke("read-economic-evaluation", payload),
+  downloadReportFiles: (payload) => electron.ipcRenderer.invoke("download-report-files", payload),
+  downloadOutputTotal: (payload) => electron.ipcRenderer.invoke("download-output-total", payload)
+});
+electron.contextBridge.exposeInMainWorld("electronStore", {
+  get: (key) => electron.ipcRenderer.invoke("electron-store-get", key),
+  set: (key, value) => electron.ipcRenderer.invoke("electron-store-set", key, value),
+  delete: (key) => electron.ipcRenderer.invoke("electron-store-delete", key)
+});
